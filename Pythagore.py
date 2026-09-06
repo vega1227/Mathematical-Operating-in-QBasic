0c83c12257a773567004e14c8d02058b2ef18a0b
vega1227 <simon.duclaux@gmail.com>
2026-09-06 21:17:46 +0200

    Add Pythagorean theorem calculation script

---
+++ Pythagore.py
@@ -0,0 +1,12 @@
+
+import math
+A = float(input("Combien vaut A ? "))
+B = float(input("Combien vaut B ? "))
+A1 = A ** 2
+B1 = B ** 2
+C1 = B1 + A1
+C = math.sqrt(C1)
+print("Le résultat est : ")
+print(C)
+
+
